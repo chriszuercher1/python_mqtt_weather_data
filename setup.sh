3d6ffb85bd6508f50a29edda1d1364feea7d2e33
Mario Gellrich <mario.gellrich@gmx.ch>
2024-08-31 15:43:33 +0200

    ...

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+echo "Updating package list..."
+sudo apt-get update
+
+echo "Installing Mosquitto and clients..."
+sudo apt-get install -y mosquitto mosquitto-clients
+
+echo "Installing Python dependencies..."
+pip install -r requirements.txt
+
+echo "Setup complete!"
