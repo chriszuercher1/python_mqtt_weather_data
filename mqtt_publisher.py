21a6c0924549dc2641868dc3555ff1ba0faafe7c
Mario Gellrich <mario.gellrich@gmx.ch>
2024-09-01 17:06:39 +0200

    ...

--- mqtt_publisher.py
+++ mqtt_publisher.py
@@ -16,7 +16,7 @@ client = mqtt.Client("TemperaturePublisher")
 # Connect to the MQTT broker
 client.connect(broker, port)
 
-# Function to fetch the most recent air temperature data
+# Function to fetch the most recent air temperature data from the API
 def get_most_recent_air_temperature():
     url = "https://tecdottir.herokuapp.com/measurements/tiefenbrunnen?startDate=2024-09-01&sort=timestamp_cet%20desc&limit=500&offset=0"
     response = requests.get(url)
@@ -30,7 +30,7 @@ def get_most_recent_air_temperature():
     else:
         return None
 
-# Function to fetch and publish real-world temperature data
+# Function to fetch and publish air temperature data
 def publish_temperature_data():
     try:
         while True:
