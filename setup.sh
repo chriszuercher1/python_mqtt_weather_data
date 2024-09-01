60c577ffb5a5971ed5c5c13e94bc263e12c4cb7c
Mario Gellrich <mario.gellrich@gmx.ch>
2024-09-01 16:18:09 +0200

    ...

--- setup.sh
+++ setup.sh
@@ -7,4 +7,7 @@ sudo apt-get update
 echo "Installing Mosquitto and clients..."
 sudo apt-get install -y mosquitto mosquitto-clients
 
+echo "Starting Mosquitto in the background..."
+sudo mosquitto -d
+
 echo "Setup complete!"
